3cf4b16cbf03f049ea82002e09d76743a45b6fe0
hitman4ik85 <hitman4ik85@gmail.com>
2025-02-17 20:42:35 +0200

    HelloApi

---
+++ HelloApi/HelloApi/Endpoints/HelloEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloApi.Endpoints;
+
+public static class HelloEndpoints
+{
+    public static WebApplication AddHelloEndpoints(this WebApplication app)
+    {
+        var endpoint = app.MapGroup("/api");
+
+        endpoint.MapGet("/sayHello", () => Results.Ok("Hello!"));
+
+        return app;
+    }
+}
